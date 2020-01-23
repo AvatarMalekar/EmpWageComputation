e6da9b27630935e860e49d512a7ffd6c0cce5519
AvatarMalekar <avatarmalekar4@gmail.com>
2020-01-23 15:56:14 +0530

    REFACTORED:employee working hours fetched using function

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,28 +5,28 @@ EMPLOYEE_IS_PART_TIME=0;
 EMPOLYEE_IS_FULL_TIME=1;
 TOTAL_WORKING_DAYS_OF_MONTH=20;
 MAX_WOKING_HOURS_FOR_MONTH=100;
-
 dayCount=0;
 hoursCount=0;
+function getWorkingHours(){
+	case  $1  in
+   	$EMPLOYEE_IS_PART_TIME)
+         work_hours=4;
+         ;;
+      $EMPOLYEE_IS_FULL_TIME)
+         work_hours=8
+         ;;
+    	*)
+         work_hours=0
+         ;;
+	esac
+	echo $work_hours
+}
 while [ $dayCount -le $TOTAL_WORKING_DAYS_OF_MONTH -o $hoursCount -le $MAX_WOKING_HOURS_FOR_MONTH ]
 do
 	((dayCount++))
 	random=$((RANDOM%3))
-
-	case  $random  in
-      $EMPLOYEE_IS_PART_TIME)
-      	work_hours=4;
-         ;;
-     	$EMPOLYEE_IS_FULL_TIME)
-         work_hours=8
-         ;;
-		*)
-         work_hours=0
-         ;;
-	esac
-
+	work_hours=$( getWorkingHours $random )
 	hoursCount=$(($hoursCount+$work_hours))
-
 done
 MONTHLY_EMPLOYEE_WAGE=$(($hoursCount*$WAGE_PER_HOUR))
 echo "Monthly employee wage is :"$MONTHLY_EMPLOYEE_WAGE
