1af73ff71e0401543de510ed006dca4baec6484f
AvatarMalekar <avatarmalekar4@gmail.com>
2020-01-23 13:05:39 +0530

    ADDED:if employee is part time or full time,MODIFIED:case stmt,REFACTORED:deleted unwanted variables

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,23 +1,23 @@
 #!/bin/bash -x
 echo "--------------------------------------------WELLCOME TO EMPLOYEE WAGE COMPUTATION--------------------------------------------"
-EMPLOYEE_IS_PRESENT=1
-EMPLOYEE_IS_ABSENT=0
-WAGE_PER_HOUR=20
-FULL_DAY_HOURS=8
-DAILY_EMPLOYEE_WAGE=$((20*8))
-random=$((RANDOM%2))
-
+WAGE_PER_HOUR=20;
+EMPLOYEE_IS_PART_TIME=0;
+EMPOLYEE_IS_FULL_TIME=1;
+random=$((RANDOM%3))
 case  $random  in
-		$EMPLOYEE_IS_PRESENT)
-				echo "Daily wage is :" $DAILY_EMPLOYEE_WAGE
+		$EMPLOYEE_IS_PART_TIME)
+				work_hours=4;
+				echo "Employee is working Part time"
 				;;
-		$EMPLOYEE_IS_ABSENT)
-				 echo "Daily wage is :" 0
+		$EMPOLYEE_IS_FULL_TIME)
+				work_hours=8
+				echo "Employee is working full time"
             ;;
 		*)
-				 echo "Invalid input"
+				work_hours=0
+				echo "Emloyee is absent"
 				;;
 esac
-
-
+DAILY_EMPLOYEE_WAGE=$(($WAGE_PER_HOUR*$work_hours))
+echo "Daily employee wage is :" $DAILY_EMPLOYEE_WAGE
 
