ddf346f60cfc3229397c9f63103e093185bd5d75
AvatarMalekar <avatarmalekar4@gmail.com>
2020-01-23 15:27:22 +0530

    MODIFIED:calculated wage for month with variable daily work hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,23 +4,26 @@ WAGE_PER_HOUR=20;
 EMPLOYEE_IS_PART_TIME=0;
 EMPOLYEE_IS_FULL_TIME=1;
 TOTAL_WORKING_DAYS_OF_MONTH=20;
-random=$((RANDOM%3))
-case  $random  in
+total_work_hours_for_month=0
+for (( i=1; i<=$TOTAL_WORKING_DAYS_OF_MONTH; i++ ))
+do
+	random=$((RANDOM%3))
+	case  $random  in
 		$EMPLOYEE_IS_PART_TIME)
-				work_hours=4;
-				echo "Employee is working Part time"
-				;;
+			work_hours=4;
+			total_work_hours_for_month=$(($total_work_hours_for_month+$work_hours))
+			;;
 		$EMPOLYEE_IS_FULL_TIME)
-				work_hours=8
-				echo "Employee is working full time"
-            ;;
+			work_hours=8
+			total_work_hours_for_month=$(($total_work_hours_for_month+$work_hours))
+         ;;
 		*)
-				work_hours=0
-				echo "Emloyee is absent"
-				;;
-esac
-DAILY_EMPLOYEE_WAGE=$(($WAGE_PER_HOUR*$work_hours))
-MONTHLY_EMPLOYEE_WAGE=$(($TOTAL_WORKING_DAYS_OF_MONTH*$WAGE_PER_HOUR*$work_hours))
-echo "Daily employee wage is :" $DAILY_EMPLOYEE_WAGE
+			work_hours=0
+			total_work_hours_for_month=$(($total_work_hours_for_month+$work_hours))
+			;;
+	esac
+
+done
+MONTHLY_EMPLOYEE_WAGE=$(($total_work_hours_for_month*$WAGE_PER_HOUR))
 echo "Monthly employee wage is :"$MONTHLY_EMPLOYEE_WAGE
 
